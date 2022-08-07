315a4358ca84b1afb5ba2357f836fd0e05c81a21
Jean Herrera <jeankevinherrera@gmail.com>
2022-08-07 17:18:38 -0500

    Review 01

---
+++ js/bar.js
@@ -0,0 +1,31 @@
+const url = "data.json";
+var barsContainer = document.querySelector(".main_body-bars");
+
+var barsHtml = "";+
+
+fetch(url)
+    .then((response) => {
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }
+    })
+    .then((data) => {
+        let amountArray = [];
+        data.forEach(element => {
+            amountArray.push(element.amount);
+        });
+        let maxAmount = Math.max(...amountArray);
+        console.log(maxAmount);
+        data.forEach(element => {
+            if(element.amount === maxAmount){
+                barsHtml+= "<div class='maxAmount'"
+            }else{
+                barsHtml+= "<div"
+            }
+            let height=(element.amount*100)/maxAmount;
+            console.log(parseFloat(height).toFixed(2));
+            barsHtml+= " id='bar-"+element.day+"'><div ></div><p>"+element.day+"</p></div>";
+        });
+        barsContainer.innerHTML = barsHtml;
+    })
